63ba54d7d5c3a24d6f1b78ddba4b05de1fd4212e
Breno Moura <59323654+brenomoura13@users.noreply.github.com>
2022-03-19 02:10:22 -0300

    Ajustado interfaces e página caso não tenha se conectado a API.

--- src/pages/summoner/[name].tsx
+++ src/pages/summoner/[name].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { GetServerSideProps } from "next"
 import { NextSeo } from "next-seo"
 import Image from "next/image";
@@ -5,11 +6,10 @@ import Footer from "../../components/elements/Footer/Footer";
 import Header from "../../components/elements/Header/Header";
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-const ApiKey = process.env.RIOT_KEY
+const apiKey = process.env.RIOT_KEY
 
-type Summoner = {
+interface summoner {
   id: string;
   accountId: string;
   puuid: string;
@@ -21,7 +21,7 @@ type Summoner = {
   }
 }
 
-type SummonerDetails = {
+interface summonerDetails {
   tier: string;
   rank: string;
   leaguePoints: number;
@@ -29,22 +29,24 @@ type SummonerDetails = {
   losses: number;
 }
 
-type SummonerProps = {
-  summoner: Summoner;
-  summonerDetails: SummonerDetails;
+interface summonerMasteryInfos {
+  map?: any;
+  championId: number,
+  championLevel: number,
+  championPoints: number,
+  lastPlayTime: number 
 }
 
-const Summoner = ({ summoner, summonerDetails } : SummonerProps) => {
-  let summonerName: string = summoner.name
-  let summonerLevel: number = summoner.summonerLevel
-  let summonerIcon: number = summoner.profileIconId
-  let summonerRank: string = summonerDetails.rank
-  let summonerLeaguePoints: number = summonerDetails.leaguePoints
-  let summonerTier: string = summonerDetails.tier
-  let summonerWins: number = summonerDetails.wins
-  let summonerLosses: number = summonerDetails.losses
+interface summonerProps {
+  summoner: summoner;
+  summonerDetails: summonerDetails;
+  summonerMasterys: summonerMasteryInfos;
+}
 
-  if (summoner.status?.status_code && summoner.status.status_code === 404) {
+const Summoner = ({ summoner, summonerDetails, summonerMasterys } : summonerProps) => {
+  if (summoner.status?.status_code) {
+    switch (summoner.status.status_code) {
+    case 404:
     return (
       <>
         <NextSeo
@@ -70,7 +72,43 @@ const Summoner = ({ summoner, summonerDetails } : SummonerProps) => {
         <Footer />
       </>
     )
+    case 403:
+      return (
+        <>
+        <NextSeo
+        title="Não encontrado | Summoner Statics"
+        description='Estatísticas do invocador.'
+        />
+        <Header />
+        <div className="summoner">
+          <div className="summoner__icon">
+            <Image 
+              src={`/assets/imagens/not-found.png`} 
+              alt="Icone do invocador" 
+              width={"128px"}
+              height={"128px"}
+            />
+          </div>
+          <h1 className="summoner__name">Aplicação <b>não</b> conectada à API da Riot Gomes!</h1>
+          <Link href={`/`} passHref>
+            <button className="summoner__button">Voltar</button>
+          </Link>
+        </div>
+        <Footer />
+      </>
+      )
+    }
   }
+
+  let summonerName: string = summoner.name
+  let summonerLevel: number = summoner.summonerLevel
+  let summonerIcon: number = summoner.profileIconId
+  let summonerRank: string = summonerDetails.rank
+  let summonerLeaguePoints: number = summonerDetails.leaguePoints
+  let summonerTier: string = summonerDetails.tier
+  let summonerWins: number = summonerDetails.wins
+  let summonerLosses: number = summonerDetails.losses
+  let summonerWinRate: number = ((summonerWins / (summonerWins + summonerLosses)) * 100)
 
   return (
     <>
@@ -108,11 +146,22 @@ const Summoner = ({ summoner, summonerDetails } : SummonerProps) => {
               )}
         <div className="summoner__tier--subinfo">
         {summonerTier === 'UNRANKED' ? "" 
-        :("LP: " + summonerLeaguePoints + " - WR: " + ((summonerWins/(summonerWins+summonerLosses))*100).toFixed(2) + "%")}
+        :("LP: " + summonerLeaguePoints + " - WR: " + summonerWinRate.toFixed(2) + "%")}
         </div>
         </div>
         <h1 className="summoner__name"><b>Invocador:</b> { summonerName } </h1>
-        <h2 className="summoner__level"><b>Level</b>: { summonerLevel }</h2>
+        <h2 className="summoner__level"><b>Level</b>: { summonerLevel }</h2>      
+          {summonerMasterys.map((champion: any) => (
+            <>
+              <Image
+              src={`https://raw.communitydragon.org/json/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${champion.championId}.png`}
+              alt="Borda do Elo"
+              width={'120px'}
+              height={'120px'}
+            />
+            <h3>{ champion.championPoints }</h3>
+            </>
+          ))}        
         <Link href={`/`} passHref>
           <button className="summoner__button">Voltar</button>
         </Link>
@@ -123,20 +172,38 @@ const Summoner = ({ summoner, summonerDetails } : SummonerProps) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { name } = context.params
-  const requestSummoner = await fetch(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${ApiKey}`)
-  const dataSummoner = await requestSummoner.json()
-  const summonerId = dataSummoner.id
+  var { name } = context.params
+  var searchSummonerReq = await fetch(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`)
+  var summoner = await searchSummonerReq.json()
+  if (summoner.status?.status_code) {
+    return {
+      props: {
+        summoner: summoner,
+      }
+    }
+  }
+  var summonerId = summoner.id
   var filterBySoloQueue = []
   if (summonerId) {
-    var requestSumDetails = await fetch(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${ApiKey}`)
-    var dataSumDetails = await requestSumDetails.json()
+    var requestSumDetailsReq = await fetch(`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${apiKey}`)
+    var dataSumDetails = await requestSumDetailsReq.json()
     filterBySoloQueue = dataSumDetails.filter((obj: { queueType: string }) => obj.queueType === 'RANKED_SOLO_5x5')
+    var summonerMasterysReq = await fetch(`https://br1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerId}?api_key=${apiKey}`)
+    var summonerMasterysRes = await summonerMasterysReq.json()
+    var summonerMasterys = summonerMasterysRes.map((obj: { championId: number, championLevel: number, championPoints: number, lastPlayTime: number }) => {
+      return {
+        championId: obj.championId,
+        championLevel: obj.championLevel,
+        championPoints: obj.championPoints,
+        lastPlayTime: obj.lastPlayTime
+      }
+    })
   }
   return {
     props: {
-      summoner: dataSummoner,
-      summonerDetails: filterBySoloQueue[0] || { tier: 'UNRANKED', rank: '', leaguePoints: 0 }
+      summoner: summoner,
+      summonerDetails: filterBySoloQueue[0] || { tier: 'UNRANKED', rank: '', leaguePoints: 0 },
+      summonerMasterys: summonerMasterys.slice(0, 3) || []
     }
   }
 }
